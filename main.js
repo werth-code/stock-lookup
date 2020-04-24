59907379baf6fff4d461b4b65da2f465906f1846
your name hereMatt <m21@Matthews-iMac-2.fios-router.home>
2020-04-24 08:56:52 -0400

    'Added display data to HTML"

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 const getData = () => { 
 
-    storedInput = []
+    const storedInput = []
     
     const inputs = {
         tickerInput: document.getElementById("ticker-input"),
         sharesInput: document.getElementById("shares-input"),
         nodButton: document.getElementById("nod-button").addEventListener("click", () => { 
-            const userInput = inputs.tickerInput.value
-            storedInput.push(userInput)
+            storedInput.push([inputs.tickerInput.value, inputs.sharesInput.value])
             console.log(storedInput)
 
             displayOurData()
@@ -15,24 +14,26 @@ const getData = () => {
     }
 
     const outputs = {
-        displayData: document.getElementById("show-input"),
-
+        displayDataTicker: document.getElementById("show-input-ticker"),
+        displayDataShares: document.getElementById("show-input-shares"),
     }
 
     const displayOurData = () => {
-        outputs.displayData.innerHTML = storedInput
+        outputs.displayDataTicker.innerHTML = 'Ticker: ' + inputs.tickerInput.value 
+        outputs.displayDataShares.innerHTML = 'Shares: ' + inputs.sharesInput.value
         inputs.tickerInput.value = ""
+        inputs.sharesInput.value = ""
     }
     
-
 }
 
 getData()
 
 
 
+
 /* 
 1 We have an input box that takes in data on click.
 2 We have an array that the data is added to every time the button is clicked.
-3 We are displaying our input on the page
+3 We are displaying our input on the page a
 */
